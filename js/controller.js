8545a4eb9fd2fe34654611aa860cf652790e15db
jeonneil <jeonblanco73@gmail.com>
2017-07-13 14:19:14 +0800

    Patch Again

--- js/controller.js
+++ js/controller.js
@@ -1,10 +1,12 @@
 angular.module('starter.controllers',  [])
 .controller('TodoController', function($scope, $http){
+
+
 $scope.addMed = function(){
   var name = document.getElementById('name').value;
   var quantity = document.getElementById('quantity').value;
 
-  
+
   $http({
     url:"http://localhost/Medify/include/add.php",
     method:"POST",
@@ -19,7 +21,7 @@ $scope.addMed = function(){
     document.getElementById('quantity').value = "";
   })
 };
-  
+
 $http({
     url:"http://localhost/medify/include/getdata.php",
     method:"GET"
@@ -27,7 +29,7 @@ $http({
   .then(function(response){
     console.log(response['data']);
     $scope.medlist = response['data'];
-  })  
+  })
 
 setInterval(function(){
 $http({
@@ -39,7 +41,7 @@ $http({
     $scope.medlist = response['data'];
   })
 },1000)
-  
+
 $scope.edit = function(id, name, quantity){
   $scope.add = true;
   $scope.cancel = true;
@@ -49,19 +51,19 @@ $scope.edit = function(id, name, quantity){
 }
 $scope.save_edit = function(id, name, quantity){
 
-  
+
     $http({
     url:"http://localhost/Medify/include/edit.php",
     method:"POST",
     data:{
-    'id':id,  
+    'id':id,
     'addname':name,
     'addquantity':quantity
     }
   })
   .then(function(response){
     $scope.add = false;
-    $scope.cancel = false;  
+    $scope.cancel = false;
     console.log(response);
     document.getElementById('name').value = "";
     document.getElementById('quantity').value = "";
@@ -87,4 +89,30 @@ $scope.delete = function(id){
 
   })
 }
+})
+.controller('MyController', function($scope, $ionicModal) {
+  $ionicModal.fromTemplateUrl('my-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  // Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
 });
